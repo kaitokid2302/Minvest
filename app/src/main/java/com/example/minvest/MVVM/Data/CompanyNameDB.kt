0e2e8382b629e4a098dd87b3282e44541f7a0226
Lam <lam@Lams-Macbook.local>
2023-12-10 00:57:56 +0300

    .

--- app/src/main/java/com/example/minvest/MVVM/Data/CompanyNameDB.kt
+++ app/src/main/java/com/example/minvest/MVVM/Data/CompanyNameDB.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CompanyName::class, Invest::class, Transaction::class], version = 6)
+@Database(entities = [CompanyName::class, Invest::class, Transaction::class], version = 7)
 abstract class CompanyNameDB: RoomDatabase(){
     abstract fun getCompanyNameDAO() : CompanyNameDAO
     abstract fun getInvest(): InvestDAO
